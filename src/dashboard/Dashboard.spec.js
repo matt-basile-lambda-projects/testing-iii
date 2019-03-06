ef6d5fe0c9d45d90de09f296d0870abd78b90cf1
Matt Basile <matthewbasile2@gmail.com>
2019-03-06 17:11:06 -0500

    Added a testing in Dashboard to monitor how the content changes when our buttons are pushed

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,44 @@
-// Test away
+//Test away!
+import React from 'react';
+import { render, fireEvent, queryByText } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import renderer from 'react-test-renderer';
+import 'react-testing-library/cleanup-after-each';
+
+import Dashboard from './Dashboard'
+
+describe('Display Component', () => {
+it('matches snapshot', ()=>{
+    const snap = renderer.create(<Dashboard/>)
+    expect(snap.toJSON()).toMatchSnapshot();
+})
+describe('toggleClosed()', ()=>{
+    it('show closed on Close Gate Btn', ()=>{
+        const { getByText, queryByText } = render(<Dashboard />);
+        const closeBtn = getByText(/close gate/i);
+        expect(queryByText(/closed/i)).toBeFalsy();
+        fireEvent.click(closeBtn);
+        expect(queryByText(/closed/i)).toBeTruthy(); 
+    })
+    it('show Locked on Lock Gate Btn', ()=>{
+        const { getByText, queryByText } = render(<Dashboard />);
+        const lockBtn = getByText(/lock gate/i);
+        expect(queryByText(/ locked/i)).toBeFalsy();
+        fireEvent.click(lockBtn);
+        expect(queryByText(/locked/i)).toBeTruthy(); 
+    })
+    it('show UnLocked on UnLock Gate Btn', ()=>{
+        const { getByText, queryByText } = render(<Dashboard/>);
+        const closeBtn = getByText(/close gate/i)
+        fireEvent.click(closeBtn);
+        const lockBtn = getByText(/lock gate/i);
+        fireEvent.click(lockBtn);
+        const unlockBtn = getByText(/unlock gate/i);
+        expect(queryByText(/unlocked/i)).toBeFalsy();
+        fireEvent.click(unlockBtn);
+        expect(queryByText(/unlocked/i)).toBeTruthy(); 
+    })
+})
+  
+})
+
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,32 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import renderer from 'react-test-renderer';
+import 'react-testing-library/cleanup-after-each';
+
+// import App from '../index'
+import Display from './Display';
+import Controls from '../controls/Controls'
+
+describe('Display Component', () => {
+it('matches snapshot', ()=>{
+    const snap = renderer.create(<Display/>)
+    expect(snap.toJSON()).toMatchSnapshot();
+})
+// describe('toggleClosed()', ()=>{
+//     it('close lock', ()=>{
+//         const
+//         const { queryByText } = render(<Display />);
+//         const { getByText } = render(<Controls />);
+
+//         const button = getByText(/close gate/i);
+//         expect(queryByText(/closed/i)).toBeFalsy();
+  
+//         fireEvent.click(button);
+//         expect(queryByText(/closed/i)).toBeInTheDocument(); 
+//     })
+// })
+  
+})
+
